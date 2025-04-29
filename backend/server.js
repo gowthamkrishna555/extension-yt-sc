ba3e7b0e1e171ee2e7b5150a4b22526b76b09d97
Keerthan K <keerthank.is20@sahyadri.edu.in>
2025-04-29 17:55:41 +0530

    debugging transcript part #19

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const YouTubeTranscript = require('youtube-transcript').default;
+const { fetchTranscript } = require('youtube-transcript');
 require('dotenv').config();
 
 const app = express();
@@ -229,7 +229,7 @@ app.get('/transcript', async (req, res) => {
       return res.status(400).json({ error: "Video ID is required" });
     }
 
-    const transcriptArray = await YouTubeTranscript.fetchTranscript(videoId);
+    const transcriptArray = await fetchTranscript(videoId);
     const plainTranscript = transcriptArray.map(item => item.text).join(' ');
     
     const duration = transcriptArray.reduce((max, item) => {
